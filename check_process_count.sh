b6a3c9d0b6c6704c2ba649cec31c295017b28b47
Leander Lingens <64893800+leanderlingens@users.noreply.github.com>
2023-12-31 17:34:45 +0100

    Create check_process_count.sh

---
+++ check_process_count.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Change to the directory containing the script and source the .env file
+cd "$(dirname "$0")" &&
+	source .env
+
+# Count the number of running processes
+PROCESS_COUNT=$(/usr/bin/ps -ef | wc -l)
+
+# Check if the process count is less than or equal to the specified limit
+if [[ ${PROCESS_COUNT} -le ${PROCESS_COUNT_LIMIT} ]]; then
+	# Send a GET request using curl to the specified URL for process count
+	/usr/bin/curl --request GET --url "${PROCESS_COUNT_CURL}"
+fi
