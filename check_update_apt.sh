bef23cc4b110927f4affdc75b6f3a67b85a20ccb
Leander Lingens <64893800+leanderlingens@users.noreply.github.com>
2023-12-31 17:37:59 +0100

    Create check_update_apt.sh

---
+++ check_update_apt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Change to the directory containing the script and source the .env file
+cd "$(dirname "$0")" &&
+	source .env
+
+# Get the list of upgradable packages and count the lines, suppressing warnings
+updates=$(/usr/bin/sudo /usr/bin/apt-get update >/dev/null && /usr/bin/sudo /usr/bin/apt list --upgradable 2>/dev/null | /usr/bin/grep -c "/")
+
+# Check if there are updates available
+if [[ ${updates} -eq 0 ]]; then
+	# If no updates are available, send a curl request
+	/usr/bin/curl --request GET --url "${UPDATE_APT_CURL}"
+fi
